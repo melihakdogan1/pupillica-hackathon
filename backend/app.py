abbd9a7553303b7b9d319895be64fe5a3cc98426
Mehmet Akgündoğdu <jumbonejdat23@gmail.com>
2025-09-16 15:58:56 +0300

    backend: app.py dosyasında gerekli güncellemeler yapıldı

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Frontend URL'lerini ekleyeceğiz
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
